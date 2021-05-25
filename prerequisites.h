721a81566d9d19b22d9d53bb9b1e2b0e51cb39b3
Alex <38843286+BeAllAround@users.noreply.github.com>
2021-05-25 18:37:35 +0200

    Add files via upload

--- prerequisites.h
+++ prerequisites.h
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <memory>
 #include <iterator>
 #include <typeinfo>
 #include <utility>
+#include <algorithm>
+#include <numeric>
+#include <future>
+#include <mutex>
+
 
 #include <stdio.h>
 #include <stdlib.h>
